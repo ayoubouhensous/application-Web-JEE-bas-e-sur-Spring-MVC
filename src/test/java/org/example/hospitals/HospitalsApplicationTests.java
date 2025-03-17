b95e7d7586cdba4178baf35200e976694b0382ac
ouhensous <ayoub.ouhen@gmail.com>
2025-03-17 13:33:45 +0000

    project creation

---
+++ src/test/java/org/example/hospitals/HospitalsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hospitals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
